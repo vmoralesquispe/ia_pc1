ea0f033ed8b8c065e14001d03bb9a07c930428f1
Kirk <winstijn@hotmail.com>
2019-09-09 21:46:38 -0500

    pc1

---
+++ Assets/Scripts/Agent.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Agent : MonoBehaviour
+{    
+
+    Vector3 velocity = Vector3.zero;
+    Vector3 desired = Vector3.zero;
+    Vector3 steer = Vector3.zero;    
+
+    float maxSpeed = 5;
+    float maxSteer = 1;
+
+    Transform target;
+
+    void Update()
+    {
+        target = GameObject.Find("Target").transform;
+
+        //desired 
+        desired = -(target.position - transform.position).normalized * maxSpeed;
+        //steer
+        steer = Vector3.ClampMagnitude(desired - velocity, maxSteer);
+
+        velocity += steer * Time.deltaTime;
+        transform.position += velocity * Time.deltaTime;       
+        
+    }
+}
---
+++ Assets/Scripts/Circle.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Circle : MonoBehaviour
+{
+    float lose =5;
+    float win =1;
+    public GameObject Agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        Vector3 offset = Agent.transform.position - transform.position;
+            float sqrLen = offset.sqrMagnitude;
+
+            
+            if (sqrLen < win)
+            {
+                print("Win");
+            }
+
+            if(sqrLen < lose){
+                print("Lose");
+            }        
+    }
+
+    void OnDrawGizmos(){
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, lose);
+
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(transform.position, win);
+    }
+}
